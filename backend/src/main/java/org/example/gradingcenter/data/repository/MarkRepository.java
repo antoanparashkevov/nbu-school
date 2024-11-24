6be0df57578dda0b16d8e7cb2cc0818cda659a80
Ivo Bankov <ivobankov758@gmail.com>
2024-11-24 22:42:49 +0200

    fix imports

--- backend/src/main/java/org/example/gradingcenter/data/repository/MarkRepository.java
+++ backend/src/main/java/org/example/gradingcenter/data/repository/MarkRepository.java
@@ -1,5 +1,6 @@
 package org.example.gradingcenter.data.repository;
 
+import org.example.gradingcenter.data.entity.Mark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarkRepository extends JpaRepository<Mark, Long> {
--- backend/src/main/java/org/example/gradingcenter/data/repository/RoleRepository.java
+++ backend/src/main/java/org/example/gradingcenter/data/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package org.example.gradingcenter.data.repository;
 
+import org.example.gradingcenter.data.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
