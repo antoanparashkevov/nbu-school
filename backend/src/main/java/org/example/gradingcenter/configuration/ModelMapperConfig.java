9b1480872a3b23ba1d8c7684dbab59b948a8e11f
ivobankov758 <ivobankov758@gmail.com>
2025-03-09 12:38:46 +0200

    fix user role mapping

--- backend/src/main/java/org/example/gradingcenter/configuration/ModelMapperConfig.java
+++ backend/src/main/java/org/example/gradingcenter/configuration/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package org.example.gradingcenter.configuration;
 
+import org.example.gradingcenter.data.entity.Role;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
 
+    public static final Converter<Role, String> ROLE_TO_STRING =
+            context -> {
+                Role source = context.getSource();
+                return (source == null) ? null : source.getAuthority();
+            };
+
     @Bean
     public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        addRoleMappings(modelMapper);
         return new ModelMapper();
     }
 
-    private static void addSpecialityMappings(ModelMapper modelMapper) {
-        modelMapper.createTypeMap(Speciality.class, SpecialityDto.class).setConverter(MapConverters.SPECIALITY_TO_STRING);
+    private static void addRoleMappings(ModelMapper modelMapper) {
+        modelMapper.createTypeMap(Role.class, String.class).setConverter(ROLE_TO_STRING);
     }
 }
 
