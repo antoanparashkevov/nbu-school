3814b2fdc6da40e0cd4aa9e58dc9fa9ce436f945
“antoanparashkevov” <antoanparashkevov@gmail.com>
2025-05-17 14:54:25 +0300

    getLoggedInUser method

--- backend/src/main/java/org/example/gradingcenter/service/AuthenticationService.java
+++ backend/src/main/java/org/example/gradingcenter/service/AuthenticationService.java
@@ -1,12 +1,14 @@
 package org.example.gradingcenter.service;
 
-import org.example.gradingcenter.data.dto.users.UserLoginDto;
 import org.example.gradingcenter.data.dto.users.UserLoginResponseDto;
+import org.example.gradingcenter.data.dto.users.UserOutDto;
 import org.example.gradingcenter.data.dto.users.UserRegisterDto;
 
 public interface AuthenticationService {
 
     UserLoginResponseDto register(UserRegisterDto userRegisterDto);
+
+    UserOutDto getLoggedInUser();
 
     //UserLoginResponseDto login(UserLoginDto userLoginDto);
 
--- backend/src/main/java/org/example/gradingcenter/service/impl/AuthenticationServiceImpl.java
+++ backend/src/main/java/org/example/gradingcenter/service/impl/AuthenticationServiceImpl.java
@@ -2,20 +2,25 @@ package org.example.gradingcenter.service.impl;
 
 import lombok.AllArgsConstructor;
 import org.example.gradingcenter.configuration.ModelMapperConfig;
-import org.example.gradingcenter.data.dto.users.UserLoginDto;
 import org.example.gradingcenter.data.dto.users.UserLoginResponseDto;
+import org.example.gradingcenter.data.dto.users.UserOutDto;
 import org.example.gradingcenter.data.dto.users.UserRegisterDto;
+import org.example.gradingcenter.data.entity.Role;
 import org.example.gradingcenter.data.entity.users.User;
 import org.example.gradingcenter.data.repository.RoleRepository;
+import org.example.gradingcenter.exceptions.AuthorizationFailureException;
 import org.example.gradingcenter.service.AuthenticationService;
 import org.example.gradingcenter.service.TokenService;
 import org.example.gradingcenter.service.UserService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -47,6 +52,26 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         UserLoginResponseDto loginResponse = mapperConfig.getModelMapper().map(savedUser, UserLoginResponseDto.class);
         //loginResponse.setJwt(token);
         return loginResponse;
+    }
+
+    @Override
+    public UserOutDto getLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
+            User loggedInUser = (User) authentication.getPrincipal();
+
+            UserOutDto userOutDto = new UserOutDto();
+            userOutDto.setId(loggedInUser.getId());
+            userOutDto.setFirstName(loggedInUser.getFirstName());
+            userOutDto.setLastName(loggedInUser.getLastName());
+            userOutDto.setUsername(loggedInUser.getUsername());
+            userOutDto.setAuthorities(loggedInUser.getAuthorities().stream().map(Role::getAuthority).collect(Collectors.toSet()));
+
+            return userOutDto;
+        }
+
+        throw new AuthorizationFailureException("There is not a logged in user");
     }
 
 //    @Override
