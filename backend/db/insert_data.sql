90cd1e152c2eeec05cc3c1d3d781ce00d43bba8e
Ivo Bankov <ivobankov758@gmail.com>
2025-06-23 14:02:14 +0300

    update insert data

--- backend/db/insert_data.sql
+++ backend/db/insert_data.sql
@@ -16,45 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `grade`
---
-
-DROP TABLE IF EXISTS `grade`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `grade` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `school_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKmofvtgl3d0w6b7mclu88gxvt0` (`school_id`),
-  CONSTRAINT `FKmofvtgl3d0w6b7mclu88gxvt0` FOREIGN KEY (`school_id`) REFERENCES `school` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `grade`
 --
 
 LOCK TABLES `grade` WRITE;
 /*!40000 ALTER TABLE `grade` DISABLE KEYS */;
-INSERT INTO `grade` VALUES (1,'1A',1);
+INSERT INTO `grade` VALUES (1,'9B',1),(2,'9A',1);
 /*!40000 ALTER TABLE `grade` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `headmaster`
---
-
-DROP TABLE IF EXISTS `headmaster`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `headmaster` (
-  `id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FK5hsk7c59fl37q22qnf23atnd7` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `headmaster`
@@ -62,29 +31,9 @@ CREATE TABLE `headmaster` (
 
 LOCK TABLES `headmaster` WRITE;
 /*!40000 ALTER TABLE `headmaster` DISABLE KEYS */;
-INSERT INTO `headmaster` VALUES (3);
+INSERT INTO `headmaster` VALUES (2);
 /*!40000 ALTER TABLE `headmaster` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `mark`
---
-
-DROP TABLE IF EXISTS `mark`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `mark` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `mark` double NOT NULL,
-  `student_id` bigint NOT NULL,
-  `subject_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKcwocngy0rfmqdhqwm3qlrfamx` (`student_id`),
-  KEY `FKt6kc1aolba30ld4m8fqmcrt1q` (`subject_id`),
-  CONSTRAINT `FKcwocngy0rfmqdhqwm3qlrfamx` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`),
-  CONSTRAINT `FKt6kc1aolba30ld4m8fqmcrt1q` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `mark`
@@ -92,22 +41,9 @@ CREATE TABLE `mark` (
 
 LOCK TABLES `mark` WRITE;
 /*!40000 ALTER TABLE `mark` DISABLE KEYS */;
+INSERT INTO `mark` VALUES (6,2,10,3,16),(7,2.03,10,2,16);
 /*!40000 ALTER TABLE `mark` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `parent`
---
-
-DROP TABLE IF EXISTS `parent`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `parent` (
-  `id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FKor0no5wud03jeu63espilk64f` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `parent`
@@ -115,57 +51,9 @@ CREATE TABLE `parent` (
 
 LOCK TABLES `parent` WRITE;
 /*!40000 ALTER TABLE `parent` DISABLE KEYS */;
-INSERT INTO `parent` VALUES (5);
+INSERT INTO `parent` VALUES (14);
 /*!40000 ALTER TABLE `parent` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `parents_children`
---
-
-DROP TABLE IF EXISTS `parents_children`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `parents_children` (
-  `parent_id` bigint NOT NULL,
-  `student_id` bigint NOT NULL,
-  KEY `FKq09p9cqdh1d13t7siff6vcahc` (`student_id`),
-  KEY `FK9ia7ptb8v9hn1b9hkcv7f9848` (`parent_id`),
-  CONSTRAINT `FK9ia7ptb8v9hn1b9hkcv7f9848` FOREIGN KEY (`parent_id`) REFERENCES `parent` (`id`),
-  CONSTRAINT `FKq09p9cqdh1d13t7siff6vcahc` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `parents_children`
---
-
-LOCK TABLES `parents_children` WRITE;
-/*!40000 ALTER TABLE `parents_children` DISABLE KEYS */;
-/*!40000 ALTER TABLE `parents_children` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `program`
---
-
-DROP TABLE IF EXISTS `program`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `program` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `day_of_week` enum('FRIDAY','MONDAY','SATURDAY','SUNDAY','THURSDAY','TUESDAY','WEDNESDAY') NOT NULL,
-  `subject_sequence` int NOT NULL,
-  `grade_id` bigint NOT NULL,
-  `subject_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKkfw9r4kjhaufkcow7q0c4o8ru` (`grade_id`),
-  KEY `FKgpq4p6tu41utkw6fm9n9t9dlr` (`subject_id`),
-  CONSTRAINT `FKgpq4p6tu41utkw6fm9n9t9dlr` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`),
-  CONSTRAINT `FKkfw9r4kjhaufkcow7q0c4o8ru` FOREIGN KEY (`grade_id`) REFERENCES `grade` (`id`),
-  CONSTRAINT `program_chk_1` CHECK (((`subject_sequence` >= 1) and (`subject_sequence` <= 7)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `program`
@@ -177,75 +65,14 @@ LOCK TABLES `program` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `role`
---
-
-DROP TABLE IF EXISTS `role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `role` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `authority` enum('ADMIN','HEADMASTER','PARENT','STUDENT','TEACHER') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role`
---
-
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'ADMIN'),(2,'HEADMASTER'),(3,'PARENT'),(4,'STUDENT'),(5,'TEACHER');
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `school`
---
-
-DROP TABLE IF EXISTS `school`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `school` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `address` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `headmaster_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK251hwtk4rvkoblr76wknh8v41` (`name`),
-  UNIQUE KEY `UK6r170gufm8sfxk6jbknjuquvx` (`headmaster_id`),
-  CONSTRAINT `FK3y50n8x8fhs52g778h8pt1po7` FOREIGN KEY (`headmaster_id`) REFERENCES `headmaster` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `school`
 --
 
 LOCK TABLES `school` WRITE;
 /*!40000 ALTER TABLE `school` DISABLE KEYS */;
-INSERT INTO `school` VALUES (1,'ulica 123','vasil levski3',3);
+INSERT INTO `school` VALUES (1,'pimen zografski 38','vasil levski',2);
 /*!40000 ALTER TABLE `school` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `student`
---
-
-DROP TABLE IF EXISTS `student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `student` (
-  `absences` int DEFAULT '0',
-  `id` bigint NOT NULL,
-  `grade_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4xvaqcll34afqdd9vkydid5qo` (`grade_id`),
-  CONSTRAINT `FK4xvaqcll34afqdd9vkydid5qo` FOREIGN KEY (`grade_id`) REFERENCES `grade` (`id`),
-  CONSTRAINT `FKqytew32213tbnj8u0er377k3q` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `student`
@@ -253,29 +80,19 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (0,4,1);
+INSERT INTO `student` VALUES (4,10,1,1);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `subject`
+-- Dumping data for table `student_parents`
 --
 
-DROP TABLE IF EXISTS `subject`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `subject` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` enum('BIOLOGY','CHEMISTRY','ENGLISH','MATHEMATICS','PHYSICS') NOT NULL,
-  `grade_id` bigint NOT NULL,
-  `teacher_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKam8igh6e0xp9r1tl7d8r1wneb` (`grade_id`),
-  KEY `FKdvgvxo0oxhxeepkkwug7vg4w4` (`teacher_id`),
-  CONSTRAINT `FKam8igh6e0xp9r1tl7d8r1wneb` FOREIGN KEY (`grade_id`) REFERENCES `grade` (`id`),
-  CONSTRAINT `FKdvgvxo0oxhxeepkkwug7vg4w4` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `student_parents` WRITE;
+/*!40000 ALTER TABLE `student_parents` DISABLE KEYS */;
+INSERT INTO `student_parents` VALUES (10,14);
+/*!40000 ALTER TABLE `student_parents` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping data for table `subject`
@@ -283,25 +100,9 @@ CREATE TABLE `subject` (
 
 LOCK TABLES `subject` WRITE;
 /*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES (2,'MATHEMATICS',1,1,16),(3,'ENGLISH',1,1,16);
 /*!40000 ALTER TABLE `subject` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `teacher`
---
-
-DROP TABLE IF EXISTS `teacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `teacher` (
-  `id` bigint NOT NULL,
-  `school_id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKrg46bnmgbcccayv14naymqg3r` (`school_id`),
-  CONSTRAINT `FKlicv62vmu1ydw117bbxqhkof1` FOREIGN KEY (`id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FKrg46bnmgbcccayv14naymqg3r` FOREIGN KEY (`school_id`) REFERENCES `school` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `teacher`
@@ -309,29 +110,9 @@ CREATE TABLE `teacher` (
 
 LOCK TABLES `teacher` WRITE;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` VALUES (16,1),(21,1);
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `is_account_non_expired` bit(1) NOT NULL,
-  `is_account_non_locked` bit(1) NOT NULL,
-  `is_credentials_non_expired` bit(1) NOT NULL,
-  `is_enabled` bit(1) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
@@ -339,26 +120,9 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'adminFirstName',_binary '',_binary '',_binary '',_binary '','adminLastName','$2a$10$lGa42EaGtNGmzZFDKl6hZeZHTHFnp17iXJOzI6VO7MfsLl/uPr0e6','admin'),(3,'direkor',_binary '',_binary '',_binary '',_binary '','direkorov','$2a$10$eKBKJI/f1obIEzq4EdmS/upYascj2TYCll4CB99BnHeiCcCncftfe','direkor223'),(4,'ivo',_binary '',_binary '',_binary '',_binary '','ivov','$2a$10$TmbaPcUv3mSHTF8tvT4G3.Wbd3uH2TTqGQC7zxi/y.YSRG.QM.zOS','ivo223'),(5,'bashta',_binary '',_binary '',_binary '',_binary '','bashtatov','$2a$10$Y3cFyL1l5hVGm1piiK8rouacFq5KcNYNmSpPGHcF4X9T5ombQCI96','bashta223');
+INSERT INTO `user` VALUES (2,'Direktor',_binary '',_binary '',_binary '',_binary '','Direktorov','$2a$10$6.//vizOmjmlDkourdPytuTXtinDs1wTaFiE/vhBUB98t4vX2oxh6','direktor123'),(10,'Student1',_binary '',_binary '',_binary '',_binary '','Studentov1','$2a$10$gAQYkffueF8c3dLkN4EzJ.wh.m1z2wSP1PA3nnWmLdCR/q9p6mvc2','student123'),(14,'Roditel',_binary '',_binary '',_binary '',_binary '','Roditelov','$2a$10$8Ng/dSB8Q5xC7kQyNXFvkOKo5AFDnihPjBYLpAq6swcrRuNEVnAqC','rodi123'),(16,'Uchitelov2',_binary '',_binary '',_binary '',_binary '','Uchitelov','$2a$10$Z38KrWs2DSKIcBK0UGz2deuZfXTJG4QUI9sWNvUvGZSIC/Kb2PJ7G','uchi123'),(21,'Teacher',_binary '',_binary '',_binary '',_binary '','Teacherski','$2a$10$g/QZduF0g0vXKRY5//bmPOL4nHzW/nVv7vbogQ69h6LBE60XpEHxm','teach123');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user_authorities`
---
-
-DROP TABLE IF EXISTS `user_authorities`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_authorities` (
-  `users_id` bigint NOT NULL,
-  `authorities_id` bigint NOT NULL,
-  PRIMARY KEY (`users_id`,`authorities_id`),
-  KEY `FK1nqvgotmn9fcfarl4rnp6iu7k` (`authorities_id`),
-  CONSTRAINT `FK1nqvgotmn9fcfarl4rnp6iu7k` FOREIGN KEY (`authorities_id`) REFERENCES `role` (`id`),
-  CONSTRAINT `FKn6acb7bng2ljsl1bdwbqdfb4p` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user_authorities`
@@ -366,17 +130,9 @@ CREATE TABLE `user_authorities` (
 
 LOCK TABLES `user_authorities` WRITE;
 /*!40000 ALTER TABLE `user_authorities` DISABLE KEYS */;
-INSERT INTO `user_authorities` VALUES (1,1),(3,2),(5,3),(4,4);
+INSERT INTO `user_authorities` VALUES (2,2),(14,3),(10,4),(16,5),(21,5);
 /*!40000 ALTER TABLE `user_authorities` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'grading_center'
---
-
---
--- Dumping routines for database 'grading_center'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -387,4 +143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-23 14:56:06
+-- Dump completed on 2025-06-23 13:29:12
