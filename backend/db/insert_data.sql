e3a0513a36f1d192e7a1a237491e019aeaadc72d
Ivo Bankov <ivobankov758@gmail.com>
2025-07-22 19:04:08 +0300

    update insert_data.sql

--- backend/db/insert_data.sql
+++ backend/db/insert_data.sql
@@ -31,7 +31,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `headmaster` WRITE;
 /*!40000 ALTER TABLE `headmaster` DISABLE KEYS */;
-INSERT INTO `headmaster` VALUES (2);
+INSERT INTO `headmaster` VALUES (2),(25);
 /*!40000 ALTER TABLE `headmaster` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -41,7 +41,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `mark` WRITE;
 /*!40000 ALTER TABLE `mark` DISABLE KEYS */;
-INSERT INTO `mark` VALUES (6,2,10,3,16),(7,2.03,10,2,16);
+INSERT INTO `mark` VALUES (6,2,10,3,16),(11,5,10,6,26);
 /*!40000 ALTER TABLE `mark` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,17 +51,28 @@ UNLOCK TABLES;
 
 LOCK TABLES `parent` WRITE;
 /*!40000 ALTER TABLE `parent` DISABLE KEYS */;
-INSERT INTO `parent` VALUES (14);
+INSERT INTO `parent` VALUES (14),(24);
 /*!40000 ALTER TABLE `parent` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping data for table `programSlot`
+-- Dumping data for table `program_slot`
 --
 
-LOCK TABLES `programSlot` WRITE;
-/*!40000 ALTER TABLE `programSlot` DISABLE KEYS */;
-/*!40000 ALTER TABLE `programSlot` ENABLE KEYS */;
+LOCK TABLES `program_slot` WRITE;
+/*!40000 ALTER TABLE `program_slot` DISABLE KEYS */;
+INSERT INTO `program_slot` VALUES (1,'MONDAY',2,2),(2,'MONDAY',1,3),(3,'MONDAY',3,2),(4,'MONDAY',4,4),(5,'MONDAY',5,4),(6,'MONDAY',6,5),(31,'TUESDAY',1,2),(32,'TUESDAY',2,3),(33,'TUESDAY',3,8),(34,'TUESDAY',4,8),(35,'TUESDAY',5,9),(36,'TUESDAY',6,10),(37,'WEDNESDAY',1,11),(38,'WEDNESDAY',2,11),(39,'WEDNESDAY',3,2),(40,'WEDNESDAY',4,13),(41,'WEDNESDAY',5,7),(42,'WEDNESDAY',6,7),(43,'THURSDAY',1,7),(44,'THURSDAY',2,6),(45,'THURSDAY',3,5),(46,'THURSDAY',4,5),(47,'THURSDAY',5,3),(48,'THURSDAY',6,10),(49,'FRIDAY',1,10),(50,'FRIDAY',2,11),(51,'FRIDAY',3,11),(52,'FRIDAY',4,12),(53,'FRIDAY',5,5),(54,'FRIDAY',6,3);
+/*!40000 ALTER TABLE `program_slot` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_HEADMASTER'),(3,'ROLE_PARENT'),(4,'ROLE_STUDENT'),(5,'ROLE_TEACHER');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -80,7 +91,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (4, '022222222', 10,1,1);
+INSERT INTO `student` VALUES (4,'142778899',10,1,1),(0,'0141166778',23,1,1);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +101,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `student_parents` WRITE;
 /*!40000 ALTER TABLE `student_parents` DISABLE KEYS */;
-INSERT INTO `student_parents` VALUES (10,14);
+INSERT INTO `student_parents` VALUES (10,14),(10,24);
 /*!40000 ALTER TABLE `student_parents` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,7 +111,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `subject` WRITE;
 /*!40000 ALTER TABLE `subject` DISABLE KEYS */;
-INSERT INTO `subject` VALUES (2,'MATHEMATICS',1,1,16),(3,'ENGLISH',1,1,16);
+INSERT INTO `subject` VALUES (2,'MATHEMATICS',1,1,16),(3,'ENGLISH',1,1,16),(4,'LITERATURE',1,1,26),(5,'GEOGRAPHY',1,1,26),(6,'DRAWING',1,1,26),(7,'MUSIC',1,1,26),(8,'BULGARIAN',1,1,16),(9,'RUSSIAN',1,1,16),(10,'PHYSICS',1,1,24),(11,'CHEMISTRY',1,1,24),(12,'SPANISH',1,1,24),(13,'FRENCH',1,1,24);
 /*!40000 ALTER TABLE `subject` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +121,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `teacher` WRITE;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` VALUES (16,1),(21,1);
+INSERT INTO `teacher` VALUES (16,1),(21,1),(24,1),(26,1);
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +131,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (2,'Direktor',_binary '',_binary '',_binary '',_binary '','Direktorov','$2a$10$6.//vizOmjmlDkourdPytuTXtinDs1wTaFiE/vhBUB98t4vX2oxh6','direktor123'),(10,'Student1',_binary '',_binary '',_binary '',_binary '','Studentov1','$2a$10$gAQYkffueF8c3dLkN4EzJ.wh.m1z2wSP1PA3nnWmLdCR/q9p6mvc2','student123'),(14,'Roditel',_binary '',_binary '',_binary '',_binary '','Roditelov','$2a$10$8Ng/dSB8Q5xC7kQyNXFvkOKo5AFDnihPjBYLpAq6swcrRuNEVnAqC','rodi123'),(16,'Uchitelov2',_binary '',_binary '',_binary '',_binary '','Uchitelov','$2a$10$Z38KrWs2DSKIcBK0UGz2deuZfXTJG4QUI9sWNvUvGZSIC/Kb2PJ7G','uchi123'),(21,'Teacher',_binary '',_binary '',_binary '',_binary '','Teacherski','$2a$10$g/QZduF0g0vXKRY5//bmPOL4nHzW/nVv7vbogQ69h6LBE60XpEHxm','teach123');
+INSERT INTO `user` VALUES (1,'adminFirstName',_binary '',_binary '',_binary '',_binary '','adminLastName','$2a$10$r/J9fjOmbOXPXcZxwIVs6uofuwiTjjVP1NGoT.VilfStcSTRC305O','admin'),(2,'Direktor',_binary '',_binary '',_binary '',_binary '','Direktorov','$2a$10$6.//vizOmjmlDkourdPytuTXtinDs1wTaFiE/vhBUB98t4vX2oxh6','direktor123'),(10,'Student1',_binary '',_binary '',_binary '',_binary '','Studentov1','$2a$10$gAQYkffueF8c3dLkN4EzJ.wh.m1z2wSP1PA3nnWmLdCR/q9p6mvc2','student123'),(14,'Roditellll',_binary '',_binary '',_binary '',_binary '','Roditelovvvv','$2a$10$8Ng/dSB8Q5xC7kQyNXFvkOKo5AFDnihPjBYLpAq6swcrRuNEVnAqC','rodi123'),(16,'Uchitelov2',_binary '',_binary '',_binary '',_binary '','Uchitelov','$2a$10$Z38KrWs2DSKIcBK0UGz2deuZfXTJG4QUI9sWNvUvGZSIC/Kb2PJ7G','uchi123'),(21,'Teacher',_binary '',_binary '',_binary '',_binary '','Teacherski','$2a$10$g/QZduF0g0vXKRY5//bmPOL4nHzW/nVv7vbogQ69h6LBE60XpEHxm','teach123'),(23,'Student2',_binary '',_binary '',_binary '',_binary '','Studentov2','$2a$10$SlsYXq35ZymZdYtJX85ZDOYh036HVI7wBAVO28/9oD75WJwMwtTM6','student1234'),(24,'Teacher',_binary '',_binary '',_binary '',_binary '','Parentov','$2a$10$6.//vizOmjmlDkourdPytuTXtinDs1wTaFiE/vhBUB98t4vX2oxh6','pareacher'),(25,'Direktor2',_binary '',_binary '',_binary '',_binary '','Direktorov2','$2a$10$04rwkkXk8W7D1Hm0RW/oUu60Ohn41quQq5FwWWdoqssQKLSttCRCm','direktor77'),(26,'Tests Teacher',_binary '',_binary '',_binary '',_binary '','Test Teacherov','$2a$10$J81cCTtJHNpPbu/bjAgwQus49zpCqRxNOxZI4/G2Dic88OrME1LmK','testTeacher');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -130,7 +141,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `user_authorities` WRITE;
 /*!40000 ALTER TABLE `user_authorities` DISABLE KEYS */;
-INSERT INTO `user_authorities` VALUES (2,2),(14,3),(10,4),(16,5),(21,5);
+INSERT INTO `user_authorities` VALUES (1,1),(2,2),(25,2),(14,3),(24,3),(10,4),(23,4),(16,5),(21,5),(24,5),(26,5);
 /*!40000 ALTER TABLE `user_authorities` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -143,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-23 13:29:12
+-- Dump completed on 2025-07-22 19:02:08
